f55fc7f6c4e436e212ac866a31ceff4bfa4ca3ff
alaabdww <91311739+alaabdww@users.noreply.github.com>
2025-08-14 13:15:48 +0200

    consolecomplexiteit

--- SlnLes01HerhalingAanvulling/ConsoleComplexiteit/Program.cs
+++ SlnLes01HerhalingAanvulling/ConsoleComplexiteit/Program.cs
@@ -23,10 +23,79 @@ namespace ConsoleComplexiteit
 
         private static int AantalLettergrepen(string a)
         {
+            if (string.IsNullOrEmpty(a))
+            {
+                return 0;
+            }
+            a = a.ToLower();
+            bool vorigeWasKlinker = false;
+            string klinkers = "aeiouy";
+            int aantal = 0;
+
+            foreach (char c in a)
+            {
+                if (klinkers.Contains(c))
+                {
+                    if (vorigeWasKlinker == false)
+                    {
+                        aantal++;
+                        vorigeWasKlinker = true;
+                    }
+                    else
+                    {
+                        vorigeWasKlinker = true;
+                    }
+                }
+                else
+                {
+                    vorigeWasKlinker = false;
+                }
+            }
+
+            return aantal;
+
+
+
+        }
+
+        private static double Complexiteit(string a)
+        {
+            a = a.ToLower();
+            int lettergrepen = AantalLettergrepen(a);
+            int lengte = a.Length;
+            double complexiteit = 0;
+            int letters = 0;
+
+            foreach (char c in a)
+            {
+                if (c == 'x' || c == 'y' || c == 'q')
+                { letters++; }
+            }
+
+            complexiteit = (double)lettergrepen + (double)lengte / 3 + (double)letters;
+            complexiteit = Math.Round(complexiteit, 1);
+            return complexiteit;
 
         }
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Geef een woord (enter om te stoppen): ");
+                string woord = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(woord))
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Bedankt en tot ziens.");
+                    break;
+                }
+
+                Console.WriteLine($"aantal karakters: {woord.Length}");
+                Console.WriteLine($"aantal lettergrepen: {AantalLettergrepen(woord)}");
+                Console.WriteLine($"complexiteit: {Complexiteit(woord)}");
+                Console.WriteLine("");
+            }
         }
     }
 }
