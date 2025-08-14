6b6793f285956e09072ae3228b0fe91514289c73
alaabdww <91311739+alaabdww@users.noreply.github.com>
2025-08-14 14:02:33 +0200

    wpf

--- SlnLes01HerhalingAanvulling/WpfComplexiteit/MainWindow.xaml.cs
+++ SlnLes01HerhalingAanvulling/WpfComplexiteit/MainWindow.xaml.cs
@@ -23,6 +23,82 @@ namespace WpfComplexiteit
         public MainWindow()
         {
             InitializeComponent();
+            txtWoord.Text = null;
+        }
+
+        private bool IsKlinker(char C)
+        {
+            if (C == 'a' || C == 'e' || C == 'i' || C == 'o' || C == 'u' || C == 'y')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int AantalLettergrepen(string a)
+        {
+            if (string.IsNullOrEmpty(a))
+            {
+                return 0;
+            }
+            a = a.ToLower();
+            bool vorigeWasKlinker = false;
+            string klinkers = "aeiouy";
+            int aantal = 0;
+
+            foreach (char c in a)
+            {
+                if (klinkers.Contains(c))
+                {
+                    if (vorigeWasKlinker == false)
+                    {
+                        aantal++;
+                        vorigeWasKlinker = true;
+                    }
+                    else
+                    {
+                        vorigeWasKlinker = true;
+                    }
+                }
+                else
+                {
+                    vorigeWasKlinker = false;
+                }
+            }
+
+            return aantal;
+
+
+
+        }
+
+        private double Complexiteit(string a)
+        {
+            a = a.ToLower();
+            int lettergrepen = AantalLettergrepen(a);
+            int lengte = a.Length;
+            double complexiteit = 0;
+            int letters = 0;
+
+            foreach (char c in a)
+            {
+                if (c == 'x' || c == 'y' || c == 'q')
+                { letters++; }
+            }
+
+            complexiteit = (double)lettergrepen + (double)lengte / 3 + (double)letters;
+            complexiteit = Math.Round(complexiteit, 1);
+            return complexiteit;
+
+        }
+
+        private void btnAnalyseer_Click(object sender, RoutedEventArgs e)
+        {
+            string woord = txtWoord.Text;
+            txtResultaat.Text = $"aantal karakters: {woord.Length} {Environment.NewLine}aantal lettergrepen: {AantalLettergrepen(woord)} {Environment.NewLine}complexiteit: {Complexiteit(woord)}";
         }
     }
 }
